0d87d77f25463fadf05e9d6ff17d96acc212f6ec
Jay Schwane <jrschwane@uwalumni.com>
2018-07-01 22:58:58 -0500

    Updates util index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export { default as collides }     from './collides';
 export { default as createMatrix } from './createMatrix';
+export { default as findFirstRow } from './findFirstRow';
 export { default as merge }        from './merge';
 export { default as rotate }       from './rotate';
 export { default as resetPlayer }  from './resetPlayer';
