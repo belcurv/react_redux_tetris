13fd1469d1a96a0f235c75e66c1272b6e6c19caf
Jay Schwane <jrschwane@uwalumni.com>
2018-07-01 23:03:25 -0500

    Adds UPDATE_ARENA action

--- src/store/actions/gameActions.js
+++ src/store/actions/gameActions.js
@@ -1,15 +1,24 @@
 export const MERGE_PLAYER_ARENA = 'MERGE_PLAYER_ARENA';
-export const RESET_ARENA = 'RESET_ARENA';
+export const RESET_ARENA        = 'RESET_ARENA';
+export const UPDATE_ARENA       = 'UPDATE_ARENA';
 
 export function mergePlayerArena(player, arena) {
   return {
     type: MERGE_PLAYER_ARENA,
-    payload: { player, arena }
+    player,
+    arena
   };
 }
 
 export function resetArena() {
   return {
     type: RESET_ARENA
+  };
+}
+
+export function updateArena(newArena) {
+  return {
+    type: UPDATE_ARENA,
+    newArena
   };
 }
