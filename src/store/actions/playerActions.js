8761c8b2b7a2daf5664d7b71b41d2c4a0a4029fe
Jay Schwane <jrschwane@uwalumni.com>
2018-07-01 23:03:46 -0500

    Adds player actions

--- src/store/actions/playerActions.js
+++ src/store/actions/playerActions.js
@@ -2,30 +2,53 @@ export const PLAYER_ROTATE = 'PLAYER_ROTATE';
 export const PLAYER_MOVE   = 'PLAYER_MOVE';
 export const PLAYER_DROP   = 'PLAYER_DROP';
 export const PLAYER_RESET  = 'PLAYER_RESET';
+export const PLAYER_SET_X  = 'PLAYER_SET_X';
+export const UPDATE_SCORE  = 'UPDATE_SCORE';
+export const RESET_SCORE   = 'RESET_SCORE';
 
 export function playerRotate(direction) {
   return {
     type: PLAYER_ROTATE,
-    payload: direction
+    direction
   };
 }
 
-export function playerMove(direction) {
+export function playerMove(offset) {
   return {
     type: PLAYER_MOVE,
-    payload: direction
+    offset
   };
 }
 
 export function playerDrop(direction = 1) {
   return {
     type: PLAYER_DROP,
-    payload: direction
+    direction
   };
 }
 
 export function playerReset() {
   return {
     type: PLAYER_RESET
+  };
+}
+
+export function playerSetX(xPos) {
+  return {
+    type: PLAYER_SET_X,
+    xPos
+  };
+}
+
+export function updateScore(value) {
+  return {
+    type: UPDATE_SCORE,
+    value
+  };
+}
+
+export function resetScore() {
+  return {
+    type: RESET_SCORE
   };
 }
