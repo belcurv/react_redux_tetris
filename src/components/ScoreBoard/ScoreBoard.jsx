e902b4c33c8e04d26b0da59a7073a1b2c4f41b26
Jay Schwane <jrschwane@uwalumni.com>
2018-07-02 12:55:13 -0500

    Adds pauseToggle to ScoreBoard

--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ScoreBoard.css';
 
-const ScoreBoard = ({ score }) => (
+const ScoreBoard = ({ score, pauseToggle }) => (
   <div className="score-board">
     <h1 className="score-board__h1">Score: { score }</h1>
+    <button onClick={ pauseToggle }>Pause</button>
   </div>
 );
 
 ScoreBoard.propTypes = {
-  score: PropTypes.number
+  score       : PropTypes.number.isRequired,
+  pauseToggle : PropTypes.func.isRequired
 };
 
 export default ScoreBoard;
