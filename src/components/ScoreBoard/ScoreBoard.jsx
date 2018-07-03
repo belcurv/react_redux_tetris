c0bdfb6bc5ef4f72d0b6f22a44840fe2280e03ca
Jay Schwane <jrschwane@uwalumni.com>
2018-07-03 00:16:13 -0500

    Adds button to change color palette

--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ScoreBoard.css';
 
-const ScoreBoard = ({ score, pauseToggle }) => (
+const ScoreBoard = ({ score, pauseToggle, changePalette }) => (
   <div className="score-board">
     <h1 className="score-board__h1">Score: { score }</h1>
     <button onClick={ pauseToggle }>Pause</button>
+    <button onClick={ changePalette }>Change Palette</button>
   </div>
 );
 
 ScoreBoard.propTypes = {
-  score       : PropTypes.number.isRequired,
-  pauseToggle : PropTypes.func.isRequired
+  score         : PropTypes.number.isRequired,
+  pauseToggle   : PropTypes.func.isRequired,
+  changePalette : PropTypes.func.isRequired
 };
 
 export default ScoreBoard;
