94e65166c6cfc0e2c645db2bf99a4028da154237
Jay Schwane <jrschwane@uwalumni.com>
2018-07-02 22:13:33 -0500

    Adds calculateKick tests

--- test/utils/calculateKick.spec.js
+++ test/utils/calculateKick.spec.js
@@ -9,20 +9,21 @@ const arena = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 0, 0, 1],
-  [1, 1, 1, 1, 0, 0, 1]
+  [0, 1, 0, 0, 0, 0, 6],
+  [0, 0, 2, 0, 0, 0, 6],
+  [0, 0, 2, 0, 0, 0, 6],
+  [0, 0, 2, 0, 0, 0, 6],
+  [0, 0, 2, 0, 0, 0, 6],
+  [0, 0, 2, 0, 0, 0, 6]
 ];
 
 const player = {
   pos : { x : 0, y : 0 },
   matrix : [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 0]
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
   ]
 };
 
@@ -36,18 +37,29 @@ describe('calculateKick utility', () => {
     player.pos = { x : 0, y : 0 };
   });
 
-  it('should be a function', () => {
-    assert.isFunction(calculateKick);
-  });
-
   it('should return 1 when trying to rotate against left side', () => {
     player.pos.x = -2;
-    assert.equal(calculateKick(player, arena), 1);
+    assert.equal(calculateKick(player, arena), 2);
   });
 
   it('should return -1 when trying to rotate against right side', () => {
-    player.pos.x = 6;
-    assert.equal(calculateKick(player, arena), -1);
+    player.pos.x = 5;
+    assert.equal(calculateKick(player, arena), -2);
+  });
+
+  it('should return -1 when trying to rotate next to left of a piece', () => {
+    player.pos = { x: 4, y: 2 };
+    assert.equal(calculateKick(player, arena), -2);
+  });
+
+  it('should return 1 when trying to rotate next to right of a piece', () => {
+    player.pos = { x: 1, y: 2 };
+    assert.equal(calculateKick(player, arena), 1);
+  });
+
+  it('should return 0 when piece cannot fit between arena/rubble', () => {
+    player.pos = { x: 0, y: 6 };
+    assert.equal(calculateKick(player, arena), 0);
   });
 
 });
