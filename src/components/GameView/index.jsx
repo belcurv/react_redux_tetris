f5e753f7f959dcd2aa43becc0c33979b4511cfb9
Jay Schwane <jrschwane@uwalumni.com>
2018-07-11 16:49:17 -0500

    Renames component class files to index.jsx for easier importing

--- src/components/GameView/index.jsx
+++ src/components/GameView/index.jsx
@@ -4,7 +4,7 @@ import React       from 'react';
 import PropTypes   from 'prop-types';
 import { connect } from 'react-redux';
 
-import Canvas from '../Canvas/Canvas';
+import Canvas from '../Canvas';
 
 
 /* =========================== CLASS DEFINITION ============================ */
