eecae9248f52958e94eee92664bfd24913b27ea6
Jay Schwane <jrschwane@uwalumni.com>
2018-07-11 16:51:55 -0500

    Clarifies comments

--- src/utils/findFullRows.js
+++ src/utils/findFullRows.js
@@ -1,6 +1,6 @@
 /**
  * Finds full rows in arena
- * A full row means all array elements contain an integer between 1 - 7
+ * A full row means all elements contain an integer between 1 - 7
  * @param    {Array}   arena   Game arena
  * @returns  {Array}           Indices of full rows in reverse order, eg: [4, 3]
 */
@@ -8,7 +8,7 @@ const findFullRows = (arena) => {
   return arena
     .reduce((fullRows, row, ind) => {
       return row.every(value => (value > 0 && value < 8))
-        ? [ind, ...fullRows] // add new element to front of existing array
+        ? [ind, ...fullRows] // if row is full, add `ind` to front of fullRows
         : fullRows;
     }, []);
 };
