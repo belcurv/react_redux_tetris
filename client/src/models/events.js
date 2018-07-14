708e3e34bab1405b0b47872a1cb846328f699ecd
Jay Schwane <jrschwane@uwalumni.com>
2018-07-14 08:35:58 -0500

    Refactors Events class to properly use class static property

--- client/src/models/events.js
+++ client/src/models/events.js
@@ -3,53 +3,55 @@
  * To use, instantiate in a parent module:
  *    const events = new Events();
  * In a subscriber module:
- *    events.on('stateChanged', someFunction);
+ *    events.on('stateChanged', someNamedFunction);
+ * To unsubscribe:
+ *    events.off('stateChanged', someNamedFunction);
  * In an event publisher:
- *    app.events.emit('updateState', data ));
+ *    events.emit('updateState', data);
 */
 
 class Events {
-
-  static _events = new Set();
   
   /**
    * Subscribe to an event on receipt of 'eventName'.
    * Registers a callback on our _events Set, keyed to the event name.
    * @param   {String}     eventName   Name of the event to listen for
-   * @param   {Function}   fn          Callback to fire on hearing the event
+   * @param   {Function}   cb          Callback to fire on hearing the event
   */
-  on(eventName, fn) {
-    this._events.add({ eventName, fn });
+  on(eventName, cb) {
+    Events.listeners.add({ eventName, cb });
   }
-
+  
   /**
    * Unsubscribe from a specified event.
    * Removes the associated callback from our events Set.
    * @param   {String}     eventName   Name of event to stop listening for
-   * @param   {Function}   fn          Associated callback to unregister
+   * @param   {Function}   cb          Associated callback to unregister
   */
-  off(eventName, fn) {
-    this._events.forEach(event => {
-      if (event.eventName === eventName && event.fn === fn) {
-        this._events.delete(event);
+  off(eventName, cb) {
+    Events.listeners.forEach(event => {
+      if (event.eventName === eventName && event.cb === cb) {
+        Events.listeners.delete(event);
       }
     });
   }
 
   /**
-   * Publish an event 'eventName', passing 'data' to all callbacks
-   * registered to the event name in our 'events' object
+   * Emits event 'eventName', which causes all registered callbacks to be
+   * called with 'data'.
    * @param    {String}    eventName   Name of the event to listen for
    * @param    {Any}       data        Data to pass to the event's callback
   */
   emit(eventName, data) {
-    if (this._events[eventName]) {
-      this._events[eventName].forEach((fn) => {
-        fn(data);
-      });
-    }
+    Events.listeners.forEach(listener => {
+      if (listener.eventName === eventName) {
+        listener.cb(data);
+      }
+    });
   }
 
 }
+
+Events.listeners = new Set();
 
 export default Events;
